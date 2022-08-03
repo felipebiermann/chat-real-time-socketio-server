612185441b2d067c59c61940dcd96cf03fa6d035
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-03 18:15:17 -0300

    project websocket is done.

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ const io = socket(server);
 const SERVER_HOST = "localhost";
 const SERVER_PORT = 8080;
 
+io.on("connection", (socket) => {
+  console.log("[IO] Connection => Server has a new connection");
+  socket.on("chat.message", (data) => {
+    console.log("[SOCKET] Chat.message => ", data);
+    io.emit("chat.message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("[SOCKET] Disconnected => A connection was disconnected.");
+  });
+});
+
 server.listen(SERVER_PORT, SERVER_HOST, () => {
   console.log(
     `[HTTP] Listen => Server is running at http://${SERVER_HOST}:${SERVER_PORT}`
   );
-  console.log("s");
 });
